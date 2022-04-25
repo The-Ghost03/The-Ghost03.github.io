168101cb83513aca0c9b522706e552004c33fb8f
The-Ghost03 <kyoa240@gmail.com>
2022-04-25 02:39:06 +0000

    deploy

---
+++ precache-manifest.003da28553a808e652db2f257c06f47a.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0504e131261a588b0135",
+    "url": "/css/app.53edb087.css"
+  },
+  {
+    "revision": "9d656f835ff9ea23fa60",
+    "url": "/css/chunk-vendors.ebc3d68a.css"
+  },
+  {
+    "revision": "969b073ac5a3dade77e1fb0dcc862b8c",
+    "url": "/fonts/chunkfive-regular-webfont.969b073a.woff2"
+  },
+  {
+    "revision": "d353317c047a23bf1a5ad6553a91f767",
+    "url": "/fonts/chunkfive-regular-webfont.d353317c.woff"
+  },
+  {
+    "revision": "87842ff735d20691aa26899aae08606c",
+    "url": "/fonts/rubik-regular-webfont.87842ff7.woff2"
+  },
+  {
+    "revision": "9dce4d6d2b05bc2507f7a9d273739d8c",
+    "url": "/fonts/rubik-regular-webfont.9dce4d6d.woff"
+  },
+  {
+    "revision": "b7ef700a980074cc76c16e3667096295",
+    "url": "/img/Cat-list-1.b7ef700a.jpg"
+  },
+  {
+    "revision": "2bb920272500997406830e7c08018bb7",
+    "url": "/img/Cat-list-2.2bb92027.jpg"
+  },
+  {
+    "revision": "dca0d0987b8681b561588edccc2e0e03",
+    "url": "/img/Cat-list-3.dca0d098.jpg"
+  },
+  {
+    "revision": "65a4abea713e1386b50e4a3840104aa2",
+    "url": "/img/Cat-list-4.65a4abea.jpg"
+  },
+  {
+    "revision": "39193fc0f7f2c1e987343b448d8fd436",
+    "url": "/img/Schalom.39193fc0.jpg"
+  },
+  {
+    "revision": "4c54aff2c357d54670d01ce5be626b2e",
+    "url": "/img/agro.4c54aff2.jpg"
+  },
+  {
+    "revision": "9ebecbb573404f83d951d44318221112",
+    "url": "/img/bckgrd1.9ebecbb5.jpg"
+  },
+  {
+    "revision": "43cab295803bb669c785364584299a96",
+    "url": "/img/eco.43cab295.jpg"
+  },
+  {
+    "revision": "b79fa4a5c699068536b0d78ec55dde14",
+    "url": "/img/gastro.b79fa4a5.jpg"
+  },
+  {
+    "revision": "c9179c1c9878e6c5748d6c4b86f48ec3",
+    "url": "/img/guide.c9179c1c.jpg"
+  },
+  {
+    "revision": "f474121d7b811f62379be757a6417209",
+    "url": "/img/hotel1.f474121d.jpg"
+  },
+  {
+    "revision": "5343f83330eded62e8f96a0b3a48cd3a",
+    "url": "/img/jardin.5343f833.jpg"
+  },
+  {
+    "revision": "1f5b7ff830725276875146ce65f75beb",
+    "url": "/img/logo.1f5b7ff8.png"
+  },
+  {
+    "revision": "76617f76a5b3fbb0fb9c6d9228daf7e6",
+    "url": "/img/michel-henry.76617f76.jpg"
+  },
+  {
+    "revision": "95f63e7f0d11c20e963d40ff8411fb81",
+    "url": "/img/musee.95f63e7f.jpg"
+  },
+  {
+    "revision": "f812f96699c8a04af62757a0295ae28e",
+    "url": "/img/plage.f812f966.jpg"
+  },
+  {
+    "revision": "661a8cfbd0ee94c68b7f39ea22fffa68",
+    "url": "/img/produit1.661a8cfb.jpg"
+  },
+  {
+    "revision": "58d58a473ae67c5f40bcfc4e11b7838c",
+    "url": "/img/produit2.58d58a47.jpg"
+  },
+  {
+    "revision": "4f7acdae699b2524438bada39e6f75f2",
+    "url": "/img/produit3.4f7acdae.jpg"
+  },
+  {
+    "revision": "5058e21d07196143887f699c70da45fc",
+    "url": "/img/produit4.5058e21d.jpg"
+  },
+  {
+    "revision": "70f1bed3487d989d4db518665b6aa2cc",
+    "url": "/img/produit5.70f1bed3.jpg"
+  },
+  {
+    "revision": "ed2e7409e1d3b13d037f0cc0006c3d60",
+    "url": "/img/produit6.ed2e7409.jpg"
+  },
+  {
+    "revision": "95febf78d7f61cf9efc9da94d5a132ef",
+    "url": "/img/produit7.95febf78.jpg"
+  },
+  {
+    "revision": "f6e9c3c5b466be2387a0602586337144",
+    "url": "/img/produit9.f6e9c3c5.jpg"
+  },
+  {
+    "revision": "40cad9c3795fc8c961469c1198c48caa",
+    "url": "/img/undraw_Travelers_re_y25a.40cad9c3.png"
+  },
+  {
+    "revision": "14499323776e594835f30ca5e922ff0b",
+    "url": "/img/undraw_transfer_money_rywa.14499323.png"
+  },
+  {
+    "revision": "f821372b41b3a7deabf06f74697678b6",
+    "url": "/img/undraw_trip_dv9f.f821372b.png"
+  },
+  {
+    "revision": "f52f76bd22e8a3e8c34bf365b2969a03",
+    "url": "/img/zoo.f52f76bd.jpg"
+  },
+  {
+    "revision": "6fc4c667aa028881c92580fb0caa19ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0504e131261a588b0135",
+    "url": "/js/app.f7f37a56.js"
+  },
+  {
+    "revision": "9d656f835ff9ea23fa60",
+    "url": "/js/chunk-vendors.a26a7dec.js"
+  },
+  {
+    "revision": "096a7b21f1156a0807ad60dad607c75f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
